97cbc10945ded33e4c520f1b78ff7819c140d9d6
mbrunelliere <mbrunelliere@pitchboy.sc>
2021-01-10 16:45:51 +0100

    Change css for card background

--- src/components/molecules/Card.vue
+++ src/components/molecules/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card"  v-if="$props.post && $props.post.image">
-    <div class="backgroundBox" :style="`background: url(${$props.post.image});`"></div>
+    <div class="backgroundBox" :style="`background-image: url(${$props.post.image});`"></div>
     <div class="contentBox">
         <span>Likes : {{$props.post.likes.length}}</span>
         -
@@ -25,9 +25,18 @@ export default {
       width: 100%;
       flex: 32% 0 1;
       height: 350px;
-      background-color: #E5E5E5;
       border: 1px solid #eee;
       cursor: pointer;
+      .backgroundBox {
+          background-color: #E5E5E5;
+          background-size: cover;
+          position: absolute;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+      }
       .contentBox {
           position: absolute;
           display: flex;
@@ -35,6 +44,12 @@ export default {
           align-items: center;
           width: 100%;
           height: 100%;
+          background-color: rgba(229, 229, 229, .8);
+          opacity: 0;
+          transition: all 0.5s ease;
+          &:hover {
+              opacity: 1;
+          }
       }
   }
 </style>
