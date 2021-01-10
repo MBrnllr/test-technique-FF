1c2dbfc322d92259e43747b6965961763dcb5a70
mbrunelliere <mbrunelliere@pitchboy.sc>
2021-01-10 15:21:21 +0100

    Add PageHeaderComponent

---
+++ src/components/organisms/PageHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="pageHeader">
+    <h1>{{$props.title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+}
+</script>
+
+<style lang="scss">
+  #pageHeader {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: $headerBg;
+    padding: 0.5em 2em;
+    box-sizing: border-box;
+      h1 {
+        text-transform: uppercase;
+        text-align: center;
+        color:hsla(0,0%,100%,.85);
+        font-size: 1.5em;
+      }
+  }
+</style>
--- src/components/templates/Listing.vue
+++ src/components/templates/Listing.vue
@@ -1,19 +1,23 @@
 <template>
     <div id="mainContent">
         <Header />
-        <section>Hello world</section>
+        <div id="pageContent">
+            <PageHeader title="Feeds" />
+        </div>
         <Footer />
     </div>
 </template>
 
 <script>
 import Header from '@/components/organisms/Header.vue';
+import PageHeader from '@/components/organisms/PageHeader.vue';
 import Footer from '@/components/organisms/Footer.vue';
 
 export default {
     name: 'Listing',
     components: {
         Header,
+        PageHeader,
         Footer,
     },
 }
@@ -21,10 +25,15 @@ export default {
 
 <style lang="scss">
     #mainContent {
-    display: flex;
-    flex-flow: column;
-    flex: 1;
-    flex-wrap: nowrap;
-    justify-content: space-between;
+        display: flex;
+        flex-flow: column;
+        flex: 1;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+    }
+    #pageContent {
+        display: flex;
+        flex-flow: column;
+        flex: 1;
     }
 </style>
