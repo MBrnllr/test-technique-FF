d22b72ad69fea07f4f76d3373f4bd1d6be77e4f1
mbrunelliere <mbrunelliere@pitchboy.sc>
2021-01-10 14:38:16 +0100

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,25 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: [1, 4],
+    'no-unused-vars': [2, { vars: 'all', args: 'none' }],
+    'no-multi-spaces': 0,
+    'no-trailing-spaces': ['error', { ignoreComments: true }],
+    'func-call-spacing': 'off',
+    'no-var': [2],
+    'keyword-spacing': [2],
+    'space-infix-ops': [2],
+    'arrow-spacing': [2],
+    'no-shadow': [2],
+    'comma-dangle': [2, 'always-multiline'],
+    quotes: [2, 'single'],
+    'max-len': ['error', { code: 160 }],
+    'comma-spacing': [2, {
+        before: false,
+        after: true,
+    }],
+    semi: 'off',
+    'no-empty-function': [2],
   }
 }
